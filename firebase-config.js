ad388f9e63888e3dea7e081cda30090c0b992406
Louidporto <135027792+Louidporto@users.noreply.github.com>
2026-05-18 17:44:36 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,33 +1,15 @@
-import { initializeApp } from "firebase/app";
-
-import { getDatabase } from "firebase/database";
-
-
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyCRIoKEfFU5LqBkszVgeZDfC2eiJsTtvkI",
-
   authDomain: "fruto-proibido-73c54.firebaseapp.com",
-
+  databaseURL: "https://fruto-proibido-73c54-default-rtdb.firebaseio.com",
   projectId: "fruto-proibido-73c54",
-
   storageBucket: "fruto-proibido-73c54.firebasestorage.app",
-
   messagingSenderId: "1067844602315",
-
   appId: "1:1067844602315:web:b6f50d81fae62e15ee4e4e"
-
 };
 
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
 
-
-const app = initializeApp(firebaseConfig);
-
-const database = getDatabase(app);
-
-
-
-// Torna o banco global para os outros scripts
-
-window.database = database;
+// Atalho para o Banco de Dados
+const database = firebase.database();
