65776fc24335bdeff91fc6f0df1dec2ee459bfc7
Louidporto <135027792+Louidporto@users.noreply.github.com>
2026-05-18 10:05:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,33 @@
-// Configurações do Portal-Festas-Contagem
+import { initializeApp } from "firebase/app";
+
+import { getDatabase } from "firebase/database";
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAH6PXmb2_q5NJeBm-mEKB0NaOsYWAdi34",
-  authDomain: "portal-festas-contagem.firebaseapp.com",
-  databaseURL: "https://portal-festas-contagem-default-rtdb.firebaseio.com",
-  projectId: "portal-festas-contagem",
-  storageBucket: "portal-festas-contagem.firebasestorage.app",
-  messagingSenderId: "561179713421",
-  appId: "1:561179713421:web:69e682577cc787ef69e7c4",
-  measurementId: "G-XR8BGR7CFX"
+
+  apiKey: "AIzaSyCRIoKEfFU5LqBkszVgeZDfC2eiJsTtvkI",
+
+  authDomain: "fruto-proibido-73c54.firebaseapp.com",
+
+  projectId: "fruto-proibido-73c54",
+
+  storageBucket: "fruto-proibido-73c54.firebasestorage.app",
+
+  messagingSenderId: "1067844602315",
+
+  appId: "1:1067844602315:web:b6f50d81fae62e15ee4e4e"
+
 };
 
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
 
-// Atalho para o Banco de Dados
-const database = firebase.database();
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+
+
+// Torna o banco global para os outros scripts
+
+window.database = database;
